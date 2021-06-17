1248882eb95add6fc0224f1726482346a4259e2b
danielsarkwa <dhannysark@outlook.com>
2021-06-17 13:18:31 -0700

    completed task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - a simple program that prints size of various types of computer it is complied on
+ * Return - outputs digits
+ */
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
