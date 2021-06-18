b4fab241cf145530903247e81b6b9ed67ce54217
danielsarkwa <dhannysark@outlook.com>
2021-06-18 10:22:51 -0700

    completed task 0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,9 @@ int main(void)
  * Return - outputs digits
  */
 {
+/**
+ * returns with 0
+ */
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
