26ae0f94dbf1fa86de1aba5f14e5392569168591
DanielSarkwa <dannysark16@gmail.com>
2021-06-18 08:16:29 +0000

    completed task 6

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-code here...
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+char b;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (b = 'a'; b < 'g'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+
+return (0);
+}
