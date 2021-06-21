45120e37d1fa93c21a92f73473be34fbe4381303
DanielSarkwa <dannysark16@gmail.com>
2021-06-21 07:20:57 +0000

    completed projects

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,14 @@
-cide herr..
+#include <unistd.h>
+/**
+ * main - Prints
+ * Return: 1
+*/
+int main(void)
+{
+write(
+2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59
+);
+return (1);
+}
