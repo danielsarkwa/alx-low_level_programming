a8067efcc40872fef4e082daf893ec03d86a54bd
DanielSarkwa <dannysark16@gmail.com>
2021-06-23 05:17:06 +0000

    completed t7p3

---
+++ 0x02-functions_nested_loops/9-times-table.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* times_table - prints a 9 times table
+*
+*Return: void ... nothing ... nada
+*/
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar(i * j / 10 + 48);
+_putchar(i * j % 10 + 48);
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
+}
