fe49f1324b817f595c1244b067f1867026987d17
DanielSarkwa <dannysark16@gmail.com>
2021-06-23 01:01:07 +0000

    added files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints 'Holberton', followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("Hello, World!");
+return (0);
+}
