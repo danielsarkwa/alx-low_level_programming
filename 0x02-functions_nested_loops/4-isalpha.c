ccca97347352723016734852c13c4188cf8f096c
DanielSarkwa <dannysark16@gmail.com>
2021-06-23 02:37:29 +0000

    completed t4p3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: is value of a letter
+ * Return: 1 if c is a letter, lowercase or uppercase else 0 if not
+ */
+int _isalpha(int c)
+{
+if ((c <= 'a' && c >= 'z') || (c <= 'A' && c >= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
