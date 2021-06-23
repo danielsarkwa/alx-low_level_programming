a2020d29a9e3c55dc486e0ccd4b403496e121b2f
DanielSarkwa <dannysark16@gmail.com>
2021-06-23 07:10:57 +0000

    completed some tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,5 +7,27 @@
  */
 void print_times_table(int n)
 {
-
+int num1;
+int num2;
+if (n > 15 || n < 0)
+return;
+for (num1 = 0; num1 < n + 1; num1++)
+{
+for (num2 = 0; num2 < n + 1; num2++)
+{
+int product = num1 * num2;
+if (num2 > 0)
+{
+_putchar(product > 99 ? (product / 100) + '0' : ' ');
+_putchar(product > 9 ? ((product / 10) % 10) + '0' : ' ');
+}
+_putchar((product % 10) + '0');
+if (num2 < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,27 +7,14 @@
  */
 void print_to_98(int n)
 {
-int num1;
-int num2;
-if (n > 15 || n < 0)
-return;
-for (num1 = 0; num1 < n + 1; num1++)
+int limit = 98;
+int num = (n);
+int step = n > limit ? -1 : 1;
+int stop;
+while (!stop)
 {
-for (num2 = 0; num2 < n + 1; num2++)
-{
-int product = num1 * num2;
-if (num2 > 0)
-{
-_putchar(product > 99 ? (product / 100) + '0' : ' ');
-_putchar(product > 9 ? ((product / 10) % 10) + '0' : ' ');
-}
-_putchar((product % 10) + '0');
-if (num2 < n)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
+printf("%d%s", num, (num == limit ? "\n" : ", "));
+stop = num == limit;
+num += step;
 }
 }
