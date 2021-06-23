1f23039346aefbde0d3150ff16f4821d9133ab02
DanielSarkwa <dannysark16@gmail.com>
2021-06-23 06:48:26 +0000

    added files

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number to start from
+ * Return: void
+ */
+void print_times_table(int n)
+{
+
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description - prints the sum of all the multiples of 5, 3 except 1024
+ * Return: 0
+ */
+int main(void)
+{
+int i = 0;
+int sum = 0;
+while (i < 1024)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+i++;
+}
+_printf(sum);
+return (0);
+}
