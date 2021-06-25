21f994450a1df697fa704096dca6ba00bd43fe48
DanielSarkwa <dannysark16@gmail.com>
2021-06-25 08:06:40 +0000

    completes t3p4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - multiplies two integers
+ * 
+ * Return: the product of the two integers
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
