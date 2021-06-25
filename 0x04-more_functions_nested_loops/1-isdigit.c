ac39975993c01c05504d489208eb664f8df7f504
DanielSarkwa <dannysark16@gmail.com>
2021-06-25 07:59:54 +0000

    completes t1p4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: is value of a number
+ * Return: 1 if c is digit else 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
