cdf0ce69eb96f9ea6d4156a80df160eb531610d2
DanielSarkwa <dannysark16@gmail.com>
2021-06-30 06:36:57 +0000

    completed t5p4

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints second half of a string
+ * @str: variable of string array
+ * Returns: void
+ */
+void puts_half(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+i++;
+}
+for (i /= 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
